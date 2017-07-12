1f00c8ab7bb63b2f18b7504f035e96dfb7901436
Marouane RAIS <marouane.rais@sofrecom.com>
2017-07-12 16:40:12 +0100

    first commit...

--- gi-backend/src/main/java/com/ssm/oab/web/DomaineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssm.oab.web;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DomaineRepository extends CrudRepository<Domaine, String>{
-
-}
--- gi-backend/src/main/java/com/ssm/oab/web/ProjetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssm.oab.web;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjetRepository extends CrudRepository<Projet, String>{
-
-}

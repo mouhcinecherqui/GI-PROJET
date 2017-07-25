38f36f24a19349f5078c11c466aba1766edacc1e
mouhcinecherqui <mouhcinecherqui@gmail.com>
2017-07-25 17:41:02 +0100

    mc commit

--- gi-frontend/src/app/superuser/superuser.component.ts
+++ gi-frontend/src/app/superuser/superuser.component.ts
@@ -14,38 +14,7 @@ import 'rxjs/add/operator/debounceTime';
   styleUrls: ['./superuser.component.css']
 })
 export class SuperuserComponent implements OnInit {
-  formErrors = {
-    'firstName': '',
-    'lastName': '',
-    'password': '',
-    'email': '',
-    'codeAlliance': '',
-    'role': '',
-    'dmn': ''
-  };
-  validationMessages = {
-    'firstName': {
-      'required': 'firstname is required.'
-    },
-    'lastName': {
-      'required': 'lastname is required.'
-    },
-    'password': {
-      'required': 'password is required.'
-    },
-    'email': {
-      'required': 'email is required.'
-    },
-    'codeAlliance': {
-      'required': 'codeAlliance is required.'
-    },
-    'role': {
-      'required': 'role is required.'
-    },
-    'dmn': {
-      'required': 'domaine is required.'
-    }
-  };
+
   @ViewChild('giForm') giForm: NgForm;
   firstName = '';
   lastName = '';
@@ -66,7 +35,8 @@ export class SuperuserComponent implements OnInit {
   ifList = true;
   ifCreate = false;
   ifAdmin = false;
-  popupVisible = false;
+  selectedUser: User;
+
 
   public search = new FormControl();
 
@@ -88,114 +58,98 @@ export class SuperuserComponent implements OnInit {
     this.getRoles().then(roles => this.roles = roles);
     this.getProjets().then(projets => this.projets = projets);
     this.getDomaines().then(domaines => this.domaines = domaines);
-  }
-  inputValidation() {
-    if (!this.giForm) {return; }
-    const form = this.giForm.form;
+    this.form = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      password: new FormControl(''),
+      CodeAlliance: new FormControl(''),
+      email: new FormControl(''),
+      dmn: new FormControl(''),
+      role: new FormControl(''),
 
-    for (const field in this.formErrors) {
-      this.formErrors[field] = '';
-      const control = form.get(field);
-      if (control) {
-        const messages = this.validationMessages[field];
-        for (const key in control.errors) {
-          this.formErrors[field] += messages[key] + ' ';
-          return false;
-        }
-      }
-    }
-    return true;
+    });
   }
-
-  createuser() {
-    if (this.inputValidation()) {
-      this.onSubmit = function(user) {
-        this.http.post('/api/users', user).subscribe(response => response.json() as User[]);
-    };
-      console.log(User);
-    }
-  }
-  onSubmit = function(user) {
-
-
-    this.http.post('/api/users', user).subscribe(response => response.json() as User[]
-    );
-    console.log(user);
-  };
-  showInfo(user) {
-    console.log('hi', user);
-    this.popupVisible = true;
-  }
-  getRoles(): Promise<Role[]> {
-    return this.http.get('/api/roles')
-      .toPromise()
-      .then(response => response.json() as Role[]);
-  }
-
-  getUsersByDomaine(dmn: string): Promise<User[]> {
-    return this.http.get('/api/users?dmn=' + dmn)
-      .toPromise()
-      .then(response => response.json() as User[]);
-  }
-  //  getUsers(): Promise<User[]> {
-  //    return this.http.get('/api/users')
-  //      .toPromise()
-  //      .then(response => response.json() as User[]);
-  //  }
-  getDomaines(): Promise<Domaine[]> {
-    return this.http.get('/api/domaines')
-      .toPromise()
-      .then(response => response.json() as Domaine[]);
-  }
-  getProjets(): Promise<Projet[]> {
-    return this.http.get('/api/projets')
-      .toPromise()
-      .then(response => response.json() as Projet[]);
+  onSelect(user: User): void {
+    this.selectedUser = user;
   }
 
 
-  deleteUser(codeAlliance: string): Promise<void> {
-    console.log('ach w9e3 ' + codeAlliance);
-    return this.http.delete('/api/users/' + codeAlliance)
-      .toPromise()
-      .then(() => null).catch(this.handleError);
-  }
+onSubmit = function(user) {
+  this.http.post('/api/users', user).subscribe(response => response.json() as User[]
+  );
+  console.log(user);
+};
 
-  onDelete = function(codeAlliance: string) {
-    this.deleteUser(codeAlliance).then(() => null);
-    this.http.get('/api/users')
-      .toPromise()
-      .then(response => response.json() as User[]);
-  };
+getRoles(): Promise < Role[] > {
+  return this.http.get('/api/roles')
+    .toPromise()
+    .then(response => response.json() as Role[]);
+}
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
-  showCreation() {
-    this.ifCreate = true;
-    this.ifList = false;
-    this.ifAdmin = false;
-  }
+getUsersByDomaine(dmn: string): Promise < User[] > {
+  return this.http.get('/api/users?dmn=' + dmn)
+    .toPromise()
+    .then(response => response.json() as User[]);
+}
+//  getUsers(): Promise<User[]> {
+//    return this.http.get('/api/users')
+//      .toPromise()
+//      .then(response => response.json() as User[]);
+//  }
+getDomaines(): Promise < Domaine[] > {
+  return this.http.get('/api/domaines')
+    .toPromise()
+    .then(response => response.json() as Domaine[]);
+}
+getProjets(): Promise < Projet[] > {
+  return this.http.get('/api/projets')
+    .toPromise()
+    .then(response => response.json() as Projet[]);
+}
 
-  getDetails(codeAlliance) {
-    alert(codeAlliance);
-  }
 
-  showList() {
-    this.ifCreate = false;
-    this.ifList = true;
-    this.ifAdmin = false;
-    this.ngOnInit();
-  }
+deleteUser(codeAlliance: string): Promise < void> {
+  console.log('ach w9e3 ' + codeAlliance);
+  return this.http.delete('/api/users/' + codeAlliance)
+    .toPromise()
+    .then(() => null).catch(this.handleError);
+}
 
-  showAdmin() {
-    this.ifCreate = false;
-    this.ifList = false;
-    this.ifAdmin = true;
-  }
+onDelete = function(codeAlliance: string) {
+  this.deleteUser(codeAlliance).then(() => null);
+  this.http.get('/api/users')
+    .toPromise()
+    .then(response => response.json() as User[]);
+};
 
-  logoutUser() {
-  }
+  private handleError(error: any): Promise < any > {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
+}
+showCreation() {
+  this.ifCreate = true;
+  this.ifList = false;
+  this.ifAdmin = false;
+}
+
+getDetails(codeAlliance) {
+  alert(codeAlliance);
+}
+
+showList() {
+  this.ifCreate = false;
+  this.ifList = true;
+  this.ifAdmin = false;
+  this.ngOnInit();
+}
+
+showAdmin() {
+  this.ifCreate = false;
+  this.ifList = false;
+  this.ifAdmin = true;
+}
+
+logoutUser() {
+}
 }
 
